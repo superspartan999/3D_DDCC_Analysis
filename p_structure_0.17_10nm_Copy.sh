eba6eef6405f0970fee43f68746b0c534742a472
quantumqwah <claytonqwah@gmail.com>
2019-04-03 13:47:01 -0700

    created new file

--- p_structure_0.17_10nm_Copy.sh
+++ p_structure_0.17_10nm_Copy.sh
@@ -2,32 +2,32 @@
 #PBS - S /bin/bash
 
 mkdir Bias 1
-cp !(Child1) Bias1
+cp ./* ./Bias1
 
 mkdir Bias 2
-cp !(Child1) Bias2
+cp ./* ./Bias2
 
 mkdir Bias 3
-cp !(Child1) Bias3
+cp ./* ./Bias3
 
 mkdir Bias 4
-cp !(Child1) Bias4
+cp ./* ./Bias4
 
 mkdir Bias 5
-cp !(Child1) Bias5
+cp ./* ./Bias5
 
 mkdir Bias 6
-cp !(Child1) Bias6
+cp ./* ./Bias6
 
 mkdir Bias 7
-cp !(Child1) Bias7
+cp ./* ./Bias7
 
 mkdir Bias 8
-cp !(Child1) Bias8
+cp ./* ./Bias8
 
 mkdir Bias 9
-cp !(Child1) Bias9
+cp ./* ./Bias9
 
 mkdir Bias 10
-cp !(Child1) Bias10
+cp ./* ./Bias10
 
