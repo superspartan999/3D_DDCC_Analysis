a64f731a11c6867a6d266d89078db509da1df4db
quantumqwah <claytonqwah@gmail.com>
2019-05-29 19:15:57 -0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -499,11 +499,9 @@ path=pd.DataFrame(index=range(len(h)),columns={'Node','x','y','z'})
 
 for i,val in enumerate(h):
     path.iloc[i]=sorted_data.iloc[val][['Node','x','y','z']]
-
-
+   
     
 fig = plt.figure()
-
 
 ax = fig.add_subplot(111, projection='3d')
 
